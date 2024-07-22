5541d349d41d6231e74f579871874f5fbdee2ba4
schnoog <schnoog@schnoog.eu>
2024-07-22 16:31:15 +0200

    Create fix_cmcmadesimple.sh
    Fix for bug #12660 of CMSMS

---
+++ Bash/fix_cmcmadesimple.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# Fast fix for the CMSMS bug #12660 I reported in October 2023 (V2.2.18 - unti current 2.2.21)
+# http://dev.cmsmadesimple.org/bug/view/12660
+# cd into you cmsmadesimple directory and execute the following command
+# curl -sSL "https://raw.githubusercontent.com/schnoog/smallscripts/master/Bash/fix_cmcmadesimple.sh" | bash
+
+
+TOFIND="case 'output'"
+TOADD="case 'outputfilter'"
+CF=$(find . -name 'class.Smarty_CMS.php')
+
+echo "THIS REPLACES $TOFIND WITH $TOADD"
+echo "IN THE FILE $CF"
+grep "'outputfilter'" "$CF" >/dev/null && echo "--- file is already fixed --- exiting" && exit 0
+
+
+echo "Proceed? y/n"
+
+read ANSW
+
+if [ "$ANSW" != "y" ]
+then
+        echo "Your answer wasn't y , so I'm exiting" && exit 0
+fi
+
+#exit 0
+echo "Applying fix"
+
+sed -i "s/$TOFIND/$TOADD/g" "$CF"
+
+echo "Fix applied, going now check it"
+sleep 0.5
+grep "'outputfilter'" "$CF" || echo "Something failed" && exit 1
+exit 0
